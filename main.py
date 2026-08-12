ca015ed0731f720ca942ac0b57691d8b698925fa
narimosaeamirabass92-ux <narimosaeamirabass92@gmail.com>
2026-08-12 19:38:16 +0330

    Initialize Telegram bot with start command

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("My project started")
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "توکن_ربات_اینجا"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! ربات تشخیص آهنگ آماده است.")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
